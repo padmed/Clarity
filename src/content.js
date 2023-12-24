82be0f74a778d8e7e19d2abeaa5a9e01fd9b67b5
padmed <ddarchinian@gmail.com>
2023-12-24 20:45:32 +0400

    Position popover element

--- src/content.js
+++ src/content.js
@@ -1,15 +1,23 @@
 import { generateText } from "./services";
+import { getSelectionCords, isInTopHalf } from "./utils/helpers";
 
 const selection = window.getSelection();
+const { top, left, bottom, right } = getSelectionCords(selection);
 
-if (selection.rangeCount > 0) {
-  const range = selection.getRangeAt(0);
-  const boundingRect = range.getBoundingClientRect();
+const containerWidth = 350;
+const containerHeight = 200;
 
-  console.log("Selected text location:", {
-    top: boundingRect.top,
-    left: boundingRect.left,
-    bottom: boundingRect.bottom,
-    right: boundingRect.right,
-  });
-}
+const verticalPos = isInTopHalf(selection) ? bottom : top - containerHeight;
+const horizontalPos = (left + right) / 2 - containerWidth / 2;
+
+const popupContainer = document.createElement("div");
+popupContainer.style.cssText = `width: ${containerWidth}px;
+    height: ${containerHeight}px;
+    background-color: rgba(119, 119, 119, 1);
+    position: absolute;
+    z-index: 30000;
+    border-radius: 7px;
+    top: ${verticalPos}px;
+    left: ${horizontalPos}px`;
+
+document.body.insertAdjacentHTML("afterbegin", popupContainer.outerHTML);
---
+++ src/utils/helpers.js
@@ -0,0 +1,24 @@
+export const getSelectionCords = (selection) => {
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const boundingRect = range.getBoundingClientRect();
+
+    return {
+      top: boundingRect.top + window.scrollY,
+      left: boundingRect.left + window.scrollX,
+      bottom: boundingRect.bottom + window.scrollY,
+      right: boundingRect.right + window.scrollX,
+    };
+  }
+};
+
+export const isInTopHalf = (selection) => {
+  const range = selection.getRangeAt(0);
+  const boundingRect = range.getBoundingClientRect();
+
+  const verticalMidpoint = window.innerHeight / 2;
+
+  return boundingRect.top + boundingRect.height / 2 < verticalMidpoint
+    ? true
+    : false;
+};
