ee62174ad9a011d27b808d0593082aa2d140638c
padmed <ddarchinian@gmail.com>
2023-12-31 20:00:49 +0400

    Refactor code

--- src/content.js
+++ src/content.js
@@ -1,23 +1,13 @@
 import { generateText } from "./services";
-import { getSelectionCords, isInTopHalf } from "./utils/helpers";
+import popup from "./components/popup.js";
 
 const selection = window.getSelection();
-const { top, left, bottom, right } = getSelectionCords(selection);
+// generateText(selection.toString()).then((data) => {
+//   const text = data.results[0].generated_text;
+//   console.log(text);
+// });
 
-const containerWidth = 350;
-const containerHeight = 200;
+const popupContent = popup.querySelector("#popupContent");
+popupContent.innerHTML = selection.toString();
 
-const verticalPos = isInTopHalf(selection) ? bottom : top - containerHeight;
-const horizontalPos = (left + right) / 2 - containerWidth / 2;
-
-const popupContainer = document.createElement("div");
-popupContainer.style.cssText = `width: ${containerWidth}px;
-    height: ${containerHeight}px;
-    background-color: rgba(119, 119, 119, 1);
-    position: absolute;
-    z-index: 30000;
-    border-radius: 7px;
-    top: ${verticalPos}px;
-    left: ${horizontalPos}px`;
-
-document.body.insertAdjacentHTML("afterbegin", popupContainer.outerHTML);
+document.body.insertAdjacentHTML("afterbegin", popup.outerHTML);
