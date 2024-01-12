f6e426e1ad01a1e936fb3fe7f7f572b540c9daf7
padmed <ddarchinian@gmail.com>
2024-01-12 17:57:40 +0400

    Refactor code

--- src/utils/getElements.js
+++ src/utils/getElements.js
@@ -4,8 +4,18 @@ import {
   POPUP_CONTAINER_ID,
 } from "./constants";
 
+// Helper function to get an element by ID from a given root
+const getElementByIdInRoot = (root, id, errorMessage) => {
+  const element = root.getElementById(id);
+  if (!element) {
+    console.error(errorMessage);
+  }
+  return element;
+};
+
+// Returns the root container
 export const getRootContainer = () => {
-  const rootContainer = document.getElementById(`${ROOT_CONTAINER_ID}`);
+  const rootContainer = document.getElementById(ROOT_CONTAINER_ID);
   if (!rootContainer) {
     console.error("Root container not found");
     return null;
@@ -17,46 +27,29 @@ export const getRootContainer = () => {
 // Returns the root of popup shadow DOM
 export const getShadowRoot = () => {
   const rootContainer = getRootContainer();
-  if (!rootContainer) {
-    return null;
-  }
-
-  const shadowRoot = rootContainer.shadowRoot;
-  if (!shadowRoot) {
-    console.error("Root container not found");
-    return null;
-  }
-
-  return shadowRoot;
+  return rootContainer ? rootContainer.shadowRoot : null;
 };
 
 // Returns the main container of popup
 export const getPopupContainer = () => {
   const shadowRoot = getShadowRoot();
-  if (!shadowRoot) {
-    return null;
-  }
-
-  const popup = shadowRoot.querySelector(`#${POPUP_CONTAINER_ID}`);
-
-  if (!popup) {
-    console.error("Popup container not found");
-    return null;
-  }
-
-  return popup;
+  return shadowRoot
+    ? getElementByIdInRoot(
+        shadowRoot,
+        POPUP_CONTAINER_ID,
+        "Popup container not found in shadow root"
+      )
+    : null;
 };
 
+// Returns the content container of popup
 export const getPopupContent = () => {
-  const root = getShadowRoot();
-  if (!root) {
-    return null;
-  }
-
-  const popupContent = root.querySelector(`#${POPUP_CONTENT_ID}`);
-  if (!popupContent) {
-    console.error("Popup content not found");
-  }
-
-  return popupContent;
+  const shadowRoot = getShadowRoot();
+  return shadowRoot
+    ? getElementByIdInRoot(
+        shadowRoot,
+        POPUP_CONTENT_ID,
+        "Popup content not found in shadow root"
+      )
+    : null;
 };
