7b64f02fa3b87d56a4508b5bf208645389cc2e27
padmed <ddarchinian@gmail.com>
2024-01-12 17:05:44 +0400

    Refactor code

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,33 +1,6 @@
 import { getPopupContent } from "./getElements";
 import createHeading from "../components/heading";
 
-// Gets coordinates of user text selection
-export const getSelectionCords = (selection) => {
-  if (selection.rangeCount > 0) {
-    const range = selection.getRangeAt(0);
-    const boundingRect = range.getBoundingClientRect();
-
-    return {
-      top: boundingRect.top + window.scrollY,
-      left: boundingRect.left + window.scrollX,
-      bottom: boundingRect.bottom + window.scrollY,
-      right: boundingRect.right + window.scrollX,
-    };
-  }
-};
-
-// Checks whether the selection is in top or bottom half of a screen
-export const isInTopHalf = (selection) => {
-  const range = selection.getRangeAt(0);
-  const boundingRect = range.getBoundingClientRect();
-
-  const verticalMidpoint = window.innerHeight / 2;
-
-  return boundingRect.top + boundingRect.height / 2 < verticalMidpoint
-    ? true
-    : false;
-};
-
 export const writeInPopupContent = (textToWrite) => {
   const popupContent = getPopupContent();
   if (!popupContent) {
