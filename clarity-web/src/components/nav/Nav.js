0bd5e339dd8384841fdb4bcc2be4cd258fe33877
padmed <ddarchinian@gmail.com>
2024-01-26 23:45:01 +0400

    Create Navbar

--- clarity-web/src/components/nav/Nav.js
+++ clarity-web/src/components/nav/Nav.js
@@ -1,6 +1,21 @@
 import styles from "./nav.module.scss";
+import Logo from "../logo/Logo";
+import Image from "next/image";
+import WaveBGHeading from "@/public/svgs/WaveBGHeading.svg";
+
 const Nav = () => {
-  return <nav className={styles.nav}>hii</nav>;
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <Image
+        src={WaveBGHeading}
+        width={1512}
+        height={60}
+        className={styles.waveBg}
+        alt="minimalistic illustration"
+      ></Image>
+    </nav>
+  );
 };
 
 export default Nav;
