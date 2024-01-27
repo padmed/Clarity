5a30080dbceba957c963086b2825b10d4a4933de
padmed <ddarchinian@gmail.com>
2024-01-28 00:16:22 +0400

    Add page

---
+++ clarity-web/src/app/onboarding/signin/page.js
@@ -0,0 +1,15 @@
+import SignInGoogleBtn from "@/components/buttons/SignInGoogleBtn";
+import styles from "../onboarding.module.scss";
+import { libre_baskerville } from "@/styles/fonts";
+
+const SignIn = () => {
+  return (
+    <div className={`${styles.signinContainer} ${libre_baskerville.className}`}>
+      <h1 className={styles.heading}>Let&apos;s get started</h1>
+      <h4 className={styles.subHeading}>Sign in to get access immediately!</h4>
+      <SignInGoogleBtn />
+    </div>
+  );
+};
+
+export default SignIn;
