c6372814f44d3d2024079eb8b9d165de3251b7e7
padmed <ddarchinian@gmail.com>
2024-01-28 00:16:35 +0400

    Add fonts

--- clarity-web/src/styles/fonts.js
+++ clarity-web/src/styles/fonts.js
@@ -1,5 +1,6 @@
 import { Judson } from "next/font/google";
 import { Libre_Bodoni } from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 
 export const judson = Judson({
   subsets: ["latin"],
@@ -8,6 +9,12 @@ export const judson = Judson({
 });
 
 export const libre_bodoni = Libre_Bodoni({
+  subsets: ["latin"],
+  variable: "--font-libre-bodoni",
+  weight: "400",
+});
+
+export const libre_baskerville = Libre_Baskerville({
   subsets: ["latin"],
   variable: "--font-libre-bodoni",
   weight: "400",
