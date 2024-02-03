516cf43581c977ffaf7526635340e6c8ba9b9594
padmed <ddarchinian@gmail.com>
2024-02-03 17:38:18 +0400

    Add button

---
+++ clarity-web/src/components/buttons/SignInGoogleBtn.js
@@ -0,0 +1,32 @@
+"use client";
+import styles from "./buttons.module.scss";
+import Image from "next/image";
+
+const SignInGoogleBtn = () => {
+  const handleClick = () => {
+    const clarityExtensionId = "fbchmbkgldaidfgopdemgacpjnbcokhj";
+
+    chrome.runtime.sendMessage(
+      clarityExtensionId,
+      { message: "hello" },
+      function (response) {
+        if (!response.success) console.log("error");
+      }
+    );
+  };
+
+  return (
+    <button className={styles.googleSignin} onClick={handleClick}>
+      <Image
+        src={"/icons/google.svg"}
+        width={27}
+        height={30}
+        alt="google icon"
+      ></Image>
+      <span className={styles.line}></span>
+      <span>Sign In with Google</span>
+    </button>
+  );
+};
+
+export default SignInGoogleBtn;
