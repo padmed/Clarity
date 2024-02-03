19be1432606dafbf721dee4bc7a1222d1c4639bb
padmed <ddarchinian@gmail.com>
2024-02-03 17:39:21 +0400

    Refactor nav

--- clarity-web/src/components/nav/Nav.js
+++ clarity-web/src/components/nav/Nav.js
@@ -1,18 +1,18 @@
 import styles from "./nav.module.scss";
 import Logo from "../logo/Logo";
 import Image from "next/image";
-import WaveBGHeading from "@/public/svgs/WaveBGHeading.svg";
 
 const Nav = () => {
   return (
     <nav className={styles.nav}>
       <Logo />
       <Image
-        src={WaveBGHeading}
+        src={"/patterns/WaveBGHeading.svg"}
         width={1512}
         height={60}
         className={styles.waveBg}
         alt="minimalistic illustration"
+        priority
       ></Image>
     </nav>
   );
