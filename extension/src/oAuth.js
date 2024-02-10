2b6d2c35a250687449288d04237ac41e8eefee68
padmed <ddarchinian@gmail.com>
2024-02-10 21:19:29 +0400

    Add auth methods

--- extension/src/oAuth.js
+++ extension/src/oAuth.js
@@ -1,38 +1,66 @@
-const logIn = () => {
-  chrome.identity.getAuthToken({ interactive: true }, (token) => {
-    fetch("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + token)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => console.log(res))
-      .catch((e) => {
-        console.error("Fetch error:", e);
-      });
+const getAuthToken = (interactive) => {
+  return new Promise((resolve, reject) => {
+    chrome.identity.getAuthToken({ interactive: interactive }, (token) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(token);
+      }
+    });
   });
 };
 
-function logOut() {
-  chrome.identity.getAuthToken({ interactive: false }, (current_token) => {
-    if (!chrome.runtime.lastError) {
-      chrome.identity.removeCachedAuthToken({ token: current_token });
-      fetch(
-        "https://accounts.google.com/o/oauth2/revoke?token=" + current_token,
-        {
-          method: "GET",
-        }
-      )
-        .then((response) => {
-          if (response.ok) {
-            console.log("Token revoked successfully");
-          } else {
-            console.error("Error revoking token:", response.statusText);
-          }
-        })
-        .catch((e) => {
-          console.error("Fetch error:", e);
-        });
-    }
+const removeCachedAuthToken = (token) => {
+  return new Promise((resolve, reject) => {
+    chrome.identity.removeCachedAuthToken({ token: token }, () => {
+      if (!chrome.runtime.lastError) {
+        resolve();
+      } else {
+        reject(chrome.runtime.lastError);
+      }
+    });
   });
-}
+};
 
-export { logIn, logOut };
+const revokeToken = (token) => {
+  return fetch("https://accounts.google.com/o/oauth2/revoke?token=" + token, {
+    method: "GET",
+  });
+};
+
+const logInRequest = async () => {
+  try {
+    const token = await getAuthToken(true);
+    const response = await fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + token
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
+
+const logOutRequest = async () => {
+  try {
+    const token = await getAuthToken(false);
+    if (token) {
+      await removeCachedAuthToken(token);
+      const response = await revokeToken(token);
+      if (response.ok) {
+        console.log("Token revoked successfully");
+        return response;
+      } else {
+        console.error("Error revoking token:", response.statusText);
+      }
+    } else {
+      console.log("No token found");
+    }
+  } catch (error) {
+    console.error("Error logging out:", error);
+    return { ok: false };
+  }
+};
+
+export { logInRequest, logOutRequest };
