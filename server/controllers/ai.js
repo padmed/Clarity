32cb2f7e4d503a7a835ad0f30064852a635cbdec
padmed <ddarchinian@gmail.com>
2024-02-15 15:12:08 +0400

    Update endpoint handler

--- server/controllers/ai.js
+++ server/controllers/ai.js
@@ -1,14 +1,32 @@
 const aiRouter = require("express").Router();
 const generateText = require("../services/llmAI");
+const User = require("../db/models/user");
 
 aiRouter.post("/generate", async (req, res) => {
   try {
-    const { prompt } = req.body;
+    const { prompt, email } = req.body;
+    const user = await User.findOne({ email });
+    console.log(user);
+
+    if (!user) {
+      console.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user.freeTrial.tokens >= 20) {
+      console.error("Free trial expired");
+      user.freeTrial.access = false;
+      await user.save();
+      return res.status(402).json({ error: "Free trial expired" });
+    }
+
     const generatedText = await generateText(prompt);
-    return res.status(201).json(generatedText);
+    user.freeTrial.tokens++;
+    await user.save();
+    return res.status(201).json({ generatedText });
   } catch (error) {
-    console.error("Error while generating text:", error);
-    return res.status(500).json({ error: "Error while generating text" });
+    console.error("Error generating text:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 });
 
