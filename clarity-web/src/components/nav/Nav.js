dd35709eaab870f68170107d5aeefdd3f0402472
padmed <ddarchinian@gmail.com>
2024-02-25 21:15:00 +0400

    Remove illustration

--- clarity-web/src/components/nav/Nav.js
+++ clarity-web/src/components/nav/Nav.js
@@ -1,19 +1,10 @@
 import styles from "./nav.module.scss";
 import Logo from "../logo/Logo";
-import Image from "next/image";
 
 const Nav = () => {
   return (
     <nav className={styles.nav}>
       <Logo />
-      <Image
-        src={"/patterns/WaveBGHeading.svg"}
-        width={1512}
-        height={60}
-        className={styles.waveBg}
-        alt="minimalistic illustration"
-        priority
-      ></Image>
     </nav>
   );
 };
