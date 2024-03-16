531cebeb5a0d545bec74aa98f60ea06092cb14a8
padmed <ddarchinian@gmail.com>
2024-03-16 19:12:37 +0400

    Style page

--- clarity-web/src/app/onboarding/signin/page.js
+++ clarity-web/src/app/onboarding/signin/page.js
@@ -4,7 +4,9 @@ import { libre_baskerville } from "@/styles/fonts";
 
 const SignIn = () => {
   return (
-    <div className={`${styles.signinContainer} ${libre_baskerville.className}`}>
+    <div
+      className={`${styles.signinContainer} ${styles.container} ${libre_baskerville.className}`}
+    >
       <h1 className={styles.heading}>Let&apos;s get started</h1>
       <h4 className={styles.subHeading}>Sign in to get access immediately!</h4>
       <SignInGoogleBtn />
