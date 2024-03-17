9bdad8ab650ccd5b4ee3f4647c3d55242493f159
padmed <ddarchinian@gmail.com>
2024-03-17 14:04:34 +0400

    Style page

--- clarity-web/src/components/buttons/SignInGoogleBtn.js
+++ clarity-web/src/components/buttons/SignInGoogleBtn.js
@@ -2,34 +2,52 @@
 import styles from "./buttons.module.scss";
 import Image from "next/image";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import { libre_baskerville } from "@/styles/fonts";
+import { useState } from "react";
 
 const SignInGoogleBtn = () => {
+  const [loading, setLoading] = useState(false);
+
   const handleClick = () => {
     const clarityExtensionId = "mfbigjpknmeflcogckmjhpghdjbfpmle";
+    setLoading(true);
 
     chrome.runtime.sendMessage(
       clarityExtensionId,
       { message: "signInRequest" },
       function (response) {
-        if (!response.success) console.log("error");
+        if (!response.success) {
+          console.log("Error signing in with Google");
+        } else {
+          console.log("Signed in with Google");
+        }
+        setLoading(false);
       }
     );
   };
 
   return (
     <Button
-      className={`${styles.googleSignin} ${libre_baskerville.className}`}
+      className={`${styles.googleSignin} ${
+        loading ? styles.loading : styles.notLoading
+      } ${libre_baskerville.className}`}
       onClick={handleClick}
     >
-      <Image
-        src={"/icons/google.svg"}
-        width={27}
-        height={30}
-        alt="google icon"
-      ></Image>
-      <span className={styles.line}></span>
-      <span>Sign In with Google</span>
+      {loading ? (
+        <CircularProgress size={30} className={styles.progress} />
+      ) : (
+        <>
+          <Image
+            src={"/icons/google.svg"}
+            width={27}
+            height={30}
+            alt="google icon"
+          ></Image>
+          <span className={styles.line}></span>
+          <span>Sign In with Google</span>
+        </>
+      )}
     </Button>
   );
 };
