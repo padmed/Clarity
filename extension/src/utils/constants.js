387631afdbb4ad75f9f065185b9e4a8bde8eaa12
padmed <ddarchinian@gmail.com>
2024-03-18 22:58:00 +0400

    Refactor code

---
+++ extension/src/utils/constants.js
@@ -0,0 +1,6 @@
+// Urls
+const BASE_URL = "http://localhost:3000";
+const ONBOARDING_SIGNIN_URL = `${BASE_URL}/onboarding/signin`;
+const ONBOARDING_SUCCESS_URL = `${BASE_URL}/onboarding/success`;
+
+export { BASE_URL, ONBOARDING_SIGNIN_URL, ONBOARDING_SUCCESS_URL };
