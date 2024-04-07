91bc478235e8e987a956c3b36bbc8f50bd8e7e34
padmed <ddarchinian@gmail.com>
2024-04-07 12:48:35 +0400

    Refactor component

--- clarity-web/src/components/buttons/CtaButton.js
+++ clarity-web/src/components/buttons/CtaButton.js
@@ -1,33 +1,50 @@
 "use client";
-
 import styles from "./buttons.module.scss";
 import Button from "@mui/material/Button";
 import { libre_baskerville } from "@/styles/fonts";
+import { useEffect, useState } from "react";
 
-const CtaVariant1 = () => {
-  const handleClick = () => {
-    window.open(
-      "https://chrome.google.com/webstore/detail/clarity/mfbigjpknmeflcogckmjhpghdjbfpmle"
-    );
-  };
+const CtaButton = ({ variant, children, size, position }) => {
+  const [sizeClassname, setSizeClassname] = useState("");
+  const [variantClassname, setVariantClassname] = useState("");
+
+  useEffect(() => {
+    switch (size) {
+      case "s":
+        setSizeClassname(styles.small);
+        break;
+      case "m":
+        setSizeClassname(styles.medium);
+        break;
+      case "l":
+        setSizeClassname(styles.large);
+        break;
+      default:
+        setSizeClassname(styles.medium);
+        break;
+    }
+
+    switch (variant) {
+      case "red":
+        setVariantClassname(styles.buttonRed);
+        break;
+      case "white":
+        setVariantClassname(styles.buttonWhite);
+        break;
+      default:
+        setVariantClassname(styles.buttonRed);
+        break;
+    }
+  }, []);
 
   return (
     <Button
-      className={`${styles.buttonRed} ${libre_baskerville.className} ${styles.getOnChrome}`}
-      onClick={handleClick}
+      style={{ ...position }}
+      className={` ${styles.button} ${variantClassname} ${libre_baskerville.className} ${styles.getOnChrome} ${sizeClassname}`}
     >
-      <span>Get it on Chrome</span>
+      {children}
     </Button>
   );
-};
-
-const CtaButton = (variant) => {
-  switch (variant) {
-    case 1:
-      return <CtaVariant1 />;
-    default:
-      return <CtaVariant1></CtaVariant1>;
-  }
 };
 
 export default CtaButton;
