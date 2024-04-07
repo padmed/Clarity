6a8aff0d75f3065f6759d8320c0c40c80fbc992e
padmed <ddarchinian@gmail.com>
2024-04-07 12:49:12 +0400

    Work on Landing Page

---
+++ clarity-web/src/app/landing/page.js
@@ -0,0 +1,10 @@
+// Import any necessary modules or libraries
+import Hero from "./hero/Hero";
+
+// Define your page component
+const LandingPage = () => {
+  return <Hero></Hero>;
+};
+
+// Export the page component
+export default LandingPage;
